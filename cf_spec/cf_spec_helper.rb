91dddf54b7785f1d228eeb5dd9177bc7a786ecc7
Dave Goddard <dave@goddard.id.au>
2018-01-02 11:06:55 -0400

    Add rspec retry for integration tests

--- cf_spec/cf_spec_helper.rb
+++ cf_spec/cf_spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'machete'
 require 'machete/matchers'
+require 'rspec/retry'
 
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
@@ -11,6 +12,10 @@ RSpec.configure do |config|
 
   config.filter_run_excluding :cached => ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding :uncached => ENV['BUILDPACK_MODE'] == 'cached'
+
+  config.verbose_retry = true
+  config.default_retry_count = 2
+  config.default_sleep_interval = 5
 end
 
 def deploy_app(app_name, config)
