52a44a4c16dc5d0660e15d42264794fa9ef70ca2
Dave Goddard <dave@goddard.id.au>
2018-01-17 09:49:41 -0400

    wip

--- src/php/integration/composer_can_configure_github_oauth_token_test.go
+++ src/php/integration/composer_can_configure_github_oauth_token_test.go
@@ -11,7 +11,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = FDescribe("Composer", func() {
+var _ = Describe("Composer", func() {
 	var app *cutlass.App
 	AfterEach(func() { app = DestroyApp(app) })
 
